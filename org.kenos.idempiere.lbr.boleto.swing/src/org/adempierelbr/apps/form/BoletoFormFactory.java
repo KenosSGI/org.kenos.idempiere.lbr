77512267427ed3e7d7825fa4d455909f57a27631
Ricardo Santana <rsantana@kenos.com.br>
2017-08-28 16:08:37 -0300

    Form de geração de boletos

---
+++ org.kenos.idempiere.lbr.boleto.swing/src/org/adempierelbr/apps/form/BoletoFormFactory.java
@@ -0,0 +1,24 @@
+package org.adempierelbr.apps.form;
+
+import org.adempiere.ui.swing.factory.IFormFactory;
+import org.compiere.apps.form.FormPanel;
+
+/**
+ * 		Boleto form factory
+ * 
+ * 	@author Ricardo Santana (Kenos, www.kenos.com.br)
+ *	@version $Id: BoletoFormFactory.java, v1.0 2017/08/28 13:06:59 PM, ralexsander Exp $
+ */
+public class BoletoFormFactory implements IFormFactory
+{
+	/**
+	 * 	Try to find the form 
+	 */
+	@Override
+	public FormPanel newFormInstance (String formName)
+	{
+		if (GenBilling.GEN_BILLING_SWING.equals(formName))
+			return new VGenBilling();
+		return null;
+	}	//	newFormInstance
+}	//	BoletoFormFactory
