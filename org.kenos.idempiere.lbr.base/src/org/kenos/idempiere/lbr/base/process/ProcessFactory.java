768be96aa919bf5d99c6184b703ce1e2f0e18127
Ricardo Santana <rsantana@kenos.com.br>
2017-09-04 22:57:27 -0300

    Processo de Comissão

---
+++ org.kenos.idempiere.lbr.base/src/org/kenos/idempiere/lbr/base/process/ProcessFactory.java
@@ -0,0 +1,21 @@
+package org.kenos.idempiere.lbr.base.process;
+
+import org.adempiere.base.IProcessFactory;
+import org.compiere.process.ProcessCall;
+
+/**
+ * 		Callout Factory
+ * 
+ * 	@author Ricardo Santana (Kenos, www.kenos.com.br)
+ *	@version $Id: ProcessFactory.java, v1.0 2017/09/04 5:06:32 PM, ralexsander Exp $
+ */
+public class ProcessFactory implements IProcessFactory
+{
+	@Override
+	public ProcessCall newProcessInstance (String className)
+	{
+		if ("org.compiere.process.CommissionCalc".equals(className))
+			return new CommissionCalc ();
+		return null;
+	}
+}	//	CalloutFactory
