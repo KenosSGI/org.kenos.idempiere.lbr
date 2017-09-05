05634786afc674a37c59dd77794044cfcc3a534c
Ricardo Santana <rsantana@kenos.com.br>
2017-09-05 14:50:46 -0300

    Permitir reativar a ARM

--- org.kenos.idempiere.lbr.base/src/org/kenos/idempiere/lbr/base/model/MRMA.java
+++ org.kenos.idempiere.lbr.base/src/org/kenos/idempiere/lbr/base/model/MRMA.java
@@ -4,7 +4,10 @@ import java.sql.ResultSet;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Properties;
+import java.util.logging.Level;
 
+import org.compiere.model.ModelValidationEngine;
+import org.compiere.model.ModelValidator;
 import org.compiere.process.DocOptions;
 import org.compiere.process.DocumentEngine;
 
@@ -30,6 +33,29 @@ public class MRMA extends org.compiere.model.MRMA implements DocOptions
 	{
 		super (ctx, rs, trxName);
 	}	//	MRMA
+	
+	/**
+	 * 	Re-activate
+	 * 	@return true if success
+	 */
+	@Override
+	public boolean reActivateIt()
+	{
+		if (log.isLoggable(Level.INFO)) log.info("reActivateIt - " + toString());
+		// Before reActivate
+		String m_processMsg = ModelValidationEngine.get().fireDocValidate(this,ModelValidator.TIMING_BEFORE_REACTIVATE);
+		if (m_processMsg != null)
+			return false;
+
+		setProcessed(false);
+		
+		// After reActivate
+		m_processMsg = ModelValidationEngine.get().fireDocValidate(this,ModelValidator.TIMING_AFTER_REACTIVATE);
+		if (m_processMsg != null)
+			return false;
+
+		return true;
+	}	//	reActivateIt
 
 	/**
 	 * 	Add custom document options
