1e4b96dd771951b2b75324329a7db3b2bba09b9f
Ricardo Santana <rsantana@kenos.com.br>
2017-09-06 16:59:19 -0300

    LocationDialog para versão web

---
+++ org.kenos.idempiere.lbr.base.zk/src/org/kenos/idempiere/lbr/base/zk/editor/EditorFactory.java
@@ -0,0 +1,26 @@
+package org.kenos.idempiere.lbr.base.zk.editor;
+
+import org.adempiere.webui.editor.WEditor;
+import org.adempiere.webui.factory.IEditorFactory;
+import org.compiere.model.GridField;
+import org.compiere.model.GridTab;
+import org.compiere.util.DisplayType;
+
+/**
+ * 		Location Dialog
+ * 
+ * 	@author Ricardo Santana (Kenos, www.kenos.com.br)
+ * 	@version $Id: EditorFactory.java, v1.0 2017/09/06 3:41:54 PM, ralexsander Exp $
+ */
+public class EditorFactory implements IEditorFactory
+{
+	@Override
+	public WEditor getEditor (GridTab gridTab, GridField gridField, boolean tableEditor)
+	{
+		if (gridField == null)
+            return null;
+		if (gridField.getDisplayType() == DisplayType.Location)
+			return new WLocationEditor(gridField);
+		return null;
+	}	//	getEditor
+}	//	EditorFactory
