230347e40aa48bd3387e1357be9f508f6cb40328
Ricardo Santana <rsantana@kenos.com.br>
2017-12-18 10:09:43 -0200

    Correção do Processo de Converter Cotação

--- org.kenos.idempiere.lbr.base/src/org/kenos/idempiere/lbr/base/process/ProcessFactory.java
+++ org.kenos.idempiere.lbr.base/src/org/kenos/idempiere/lbr/base/process/ProcessFactory.java
@@ -14,7 +14,7 @@ public class ProcessFactory implements IProcessFactory
 	@Override
 	public ProcessCall newProcessInstance (String className)
 	{
-		if (CopyOrder.PROCESS_NAME.equals (className))
+		if (org.compiere.process.CopyOrder.class.getName().equals (className))
 			return new CopyOrder ();
 		if (InvoiceGenerate.PROCESS_NAME.equals (className))
 			return new InvoiceGenerate ();
