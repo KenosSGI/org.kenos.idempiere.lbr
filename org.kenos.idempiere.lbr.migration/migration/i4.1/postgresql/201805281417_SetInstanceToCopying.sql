d36cef97d018a0aa52234f3dceb75c39ef1effcb
Marcus Garducci <mgarducci@kenos.com.br>
2018-05-28 19:34:20 -0300

    Correção para desmarcar a flag Copiar Registro do campo Instância de Atributo Alvo

---
+++ org.kenos.idempiere.lbr.migration/migration/i4.1/postgresql/201805281417_SetInstanceToCopying.sql
@@ -0,0 +1,4 @@
+-- Desmarcar a flag Copiar Registro do campo Instância de Atributo Alvo
+-- 28/05/2018 14h15min20s BRT
+UPDATE AD_Column SET IsAllowCopy='N',Updated=TO_TIMESTAMP('2018-05-28 14:15:20','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=14006
+;
