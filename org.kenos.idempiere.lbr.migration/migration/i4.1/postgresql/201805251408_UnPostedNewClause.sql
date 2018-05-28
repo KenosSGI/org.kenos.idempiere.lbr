23029b136f52b5f2b16c4981ed05b83d12d8f1e3
Marcus Garducci <mgarducci@kenos.com.br>
2018-05-28 19:46:21 -0300

    Nova cláusula para a janela Documentos Não Lançados

---
+++ org.kenos.idempiere.lbr.migration/migration/i4.1/postgresql/201805251408_UnPostedNewClause.sql
@@ -0,0 +1,7 @@
+-- Nova cláusula para janela Documentos Não Lançados
+-- 25/05/2018 12h29min58s BRT
+UPDATE AD_Tab SET WhereClause='DocStatus <> ''DR''',Updated=TO_TIMESTAMP('2018-05-25 12:29:58','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Tab_ID=662
+;
+
+SELECT Register_Migration_Script ('201805251408_UnPostedNewClause.sql') FROM DUAL
+;
