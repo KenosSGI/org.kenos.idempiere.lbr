b3023b9a1af5c8594a2813c81f98b0b50c272097
Marcus Garducci <mgarducci@kenos.com.br>
2018-06-19 18:21:05 -0300

    Fix Scripts

--- org.kenos.idempiere.lbr.migration/migration/i4.1/oracle/201805281417_SetInstanceToCopying.sql
+++ org.kenos.idempiere.lbr.migration/migration/i4.1/oracle/201805281417_SetInstanceToCopying.sql
@@ -5,3 +5,6 @@ SET DEFINE OFF
 -- 28/05/2018 14h15min20s BRT
 UPDATE AD_Column SET IsAllowCopy='N',Updated=TO_DATE('2018-05-28 14:15:20','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=14006
 ;
+
+SELECT Register_Migration_Script ('201805281417_SetInstanceToCopying.sql') FROM DUAL
+;
