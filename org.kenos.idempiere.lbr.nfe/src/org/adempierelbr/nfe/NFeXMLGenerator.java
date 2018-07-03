0f723335d35ff8205a685cc87c7b8610954e8990
Rogério Feitosa <rfeitosa@kenos.com.br>
2018-07-03 17:25:33 -0300

    Tag referente a Fatura é obrigatória para NF-e versão 3

--- org.kenos.idempiere.lbr.nfe/src/org/adempierelbr/nfe/NFeXMLGenerator.java
+++ org.kenos.idempiere.lbr.nfe/src/org/adempierelbr/nfe/NFeXMLGenerator.java
@@ -1584,7 +1584,7 @@ public class NFeXMLGenerator
 					discountAmt = Env.ZERO;
 				
 				//	Fatura
-				if (nf.getC_Invoice_ID() > 0 && nf.islbr_HasOpenItems())
+				if (nf.getC_Invoice_ID() > 0)
 				{
 					Fat fat = cobr.addNewFat();
 					String fatNo = nf.getC_Invoice().getDocumentNo();
@@ -1601,13 +1601,14 @@ public class NFeXMLGenerator
 					int dupCounter = 1;
 					
 				    //	Adiciona as duplicatas da fatura
-				    for (MLBROpenItem openItem : MLBROpenItem.getOpenItem (nf.getC_Invoice_ID(), trxName))
-				    {
-				    	Dup dup = cobr.addNewDup();
-				    	dup.setNDup(fatNo + "/" + Integer.toString (dupCounter++));
-				    	dup.setDVenc(normalize (openItem.getDueDate()));
-				    	dup.setVDup(normalize (openItem.getGrandTotal().abs()));
-					}
+					if (nf.islbr_HasOpenItems())
+					    for (MLBROpenItem openItem : MLBROpenItem.getOpenItem (nf.getC_Invoice_ID(), trxName))
+					    {
+					    	Dup dup = cobr.addNewDup();
+					    	dup.setNDup(fatNo + "/" + Integer.toString (dupCounter++));
+					    	dup.setDVenc(normalize (openItem.getDueDate()));
+					    	dup.setVDup(normalize (openItem.getGrandTotal().abs()));
+						}
 				}
 			}
 		}
