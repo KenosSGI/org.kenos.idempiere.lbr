19688f4b6c4c645051e7a2c2b278be3897e95580
Marcus Garducci <mgarducci@kenos.com.br>
2018-08-15 18:49:43 -0300

    Setar Modelo da NF para NF-e, e preencher a Organização do login no processo Consultar Disponibilidade da SEFAZ

---
+++ org.kenos.idempiere.lbr.migration/migration/i4.1/postgresql/201808141218_AddDefaultValueInProcStatus.sql
@@ -0,0 +1,11 @@
+-- Adicionar valores padrões no processo Consultar Disponibilidade da SEFAZ
+-- 14/08/2018 12h13min15s BRT
+UPDATE AD_Process_Para SET DefaultValue='@#AD_Org_ID@',Updated=TO_TIMESTAMP('2018-08-14 12:13:15','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Process_Para_ID=1120258
+;
+
+-- 14/08/2018 12h13min26s BRT
+UPDATE AD_Process_Para SET DefaultValue='55',Updated=TO_TIMESTAMP('2018-08-14 12:13:26','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Process_Para_ID=1120353
+;
+
+SELECT Register_Migration_Script ('201808141218_AddDefaultValueInProcStatus.sql') FROM DUAL
+;
