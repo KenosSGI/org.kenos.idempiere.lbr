62f3923343aa36a73bde187bdcdcfd61d9ece9ac
Marcus Garducci <mgarducci@kenos.com.br>
2018-09-12 12:05:20 -0300

    Nova estrutura da VIEW RV_BOMLINE

---
+++ org.kenos.idempiere.lbr.migration/migration/i4.1/oracle/201809121130_RVBOMLine.sql
@@ -0,0 +1,76 @@
+SET SQLBLANKLINES ON
+SET DEFINE OFF
+
+CREATE	OR REPLACE	VIEW RV_BOMLine AS 
+
+WITH RECURSIVE TR_BOMLine
+(
+    M_Product_ID,
+	M_ProductBOM_ID,
+	M_Product_BOM_ID,
+    QtyBOM,
+	Depth,
+    Tree
+)
+
+AS 
+(
+    SELECT
+        pb.M_Product_ID,
+        pb.M_Product_ID AS M_ProductBOM_ID,
+        CAST('0' AS NUMERIC) AS M_Product_BOM_ID,
+        CAST('1' AS NUMERIC) AS QtyBOM,
+        0 AS Depth,
+        '' || pb.M_Product_ID AS Tree
+        
+    FROM
+        M_Product pb 
+    
+    WHERE
+        pb.IsBOM='Y'
+        
+    UNION ALL
+    
+    SELECT
+        bl.M_Product_ID,
+        bom.M_ProductBOM_ID AS M_ProductBOM_ID,
+        bom.M_Product_BOM_ID,
+        (bl.QtyBOM * bom.BOMQty) AS QtyBOM,
+        (bl.Depth + 1) AS Depth,
+        bl.Tree || '|' || bom.M_ProductBOM_ID AS Tree
+        
+    FROM
+        M_Product_BOM bom,
+        TR_BOMline bl 
+        
+    WHERE
+        (bom.M_Product_ID = bl.M_ProductBOM_ID)
+) 
+
+SELECT
+	bl.AD_Client_ID,
+	bl.AD_Org_ID,
+	bl.CreatedBy,
+	bl.UpdatedBy,
+	bl.Updated,
+	bl.Created,
+	t.M_Product_ID,
+	bl.IsActive,
+	bl.Description,
+	t.M_ProductBOM_ID,
+	bl.Line,
+	t.QtyBOM,
+	t.Depth,
+    t.Tree
+    
+FROM
+	TR_BOMline t 
+
+INNER JOIN 
+	M_Product_BOM bl 
+	ON (t.M_Product_BOM_ID = bl.M_Product_BOM_ID)
+
+;
+
+SELECT Register_Migration_Script ('201809121130_RVBOMLine.sql') FROM DUAL
+;
