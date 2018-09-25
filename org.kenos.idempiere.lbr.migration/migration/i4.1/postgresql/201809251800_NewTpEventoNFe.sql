9212fab8aa25750236608f2a9f86c1ad0ebdeabe
Rogério Feitosa <rfeitosa@kenos.com.br>
2018-09-25 18:52:32 -0300

    Criado novos Tipos de Evento para NF-e

---
+++ org.kenos.idempiere.lbr.migration/migration/i4.1/postgresql/201809251800_NewTpEventoNFe.sql
@@ -0,0 +1,67 @@
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+-- 25/09/2018 17h34min15s BRT
+INSERT INTO AD_Ref_List (AD_Ref_List_ID,Name,AD_Reference_ID,Value,AD_Client_ID,AD_Org_ID,IsActive,Created,CreatedBy,Updated,UpdatedBy,EntityType,AD_Ref_List_UU) VALUES (1122049,'Registro de Passagem para NF-e',1120220,'610500',0,0,'Y',TO_TIMESTAMP('2018-09-25 17:34:14','YYYY-MM-DD HH24:MI:SS'),100,TO_TIMESTAMP('2018-09-25 17:34:14','YYYY-MM-DD HH24:MI:SS'),100,'LBRA','e30c3792-0610-4e87-84ad-da9f34d25a71')
+;
+
+-- 25/09/2018 18h35min59s BRT
+INSERT INTO AD_Ref_List (AD_Ref_List_ID,Name,AD_Reference_ID,Value,AD_Client_ID,AD_Org_ID,IsActive,Created,CreatedBy,Updated,UpdatedBy,EntityType,AD_Ref_List_UU) VALUES (1122050,'Registro de Passagem Automatico MDFe',1120220,'610552',0,0,'Y',TO_TIMESTAMP('2018-09-25 18:35:59','YYYY-MM-DD HH24:MI:SS'),100,TO_TIMESTAMP('2018-09-25 18:35:59','YYYY-MM-DD HH24:MI:SS'),100,'LBRA','21050b31-92cc-4174-b409-571721b5f638')
+;
+
+-- 25/09/2018 18h36min18s BRT
+INSERT INTO AD_Ref_List (AD_Ref_List_ID,Name,AD_Reference_ID,Value,AD_Client_ID,AD_Org_ID,IsActive,Created,CreatedBy,Updated,UpdatedBy,EntityType,AD_Ref_List_UU) VALUES (1122051,'Registro de Passagem Automatico MDF-e com CT-e',1120220,'610554',0,0,'Y',TO_TIMESTAMP('2018-09-25 18:36:17','YYYY-MM-DD HH24:MI:SS'),100,TO_TIMESTAMP('2018-09-25 18:36:17','YYYY-MM-DD HH24:MI:SS'),100,'LBRA','61e6a93b-9d73-47d9-8a5e-81b6bb4b7f43')
+;
+
+-- 25/09/2018 18h36min30s BRT
+INSERT INTO AD_Ref_List (AD_Ref_List_ID,Name,AD_Reference_ID,Value,AD_Client_ID,AD_Org_ID,IsActive,Created,CreatedBy,Updated,UpdatedBy,EntityType,AD_Ref_List_UU) VALUES (1122052,'MDF-e Autorizado para NF-e',1120220,'610610',0,0,'Y',TO_TIMESTAMP('2018-09-25 18:36:30','YYYY-MM-DD HH24:MI:SS'),100,TO_TIMESTAMP('2018-09-25 18:36:30','YYYY-MM-DD HH24:MI:SS'),100,'LBRA','43d2f748-d82a-4520-934b-fb120f5babe4')
+;
+
+-- 25/09/2018 18h36min46s BRT
+INSERT INTO AD_Ref_List (AD_Ref_List_ID,Name,AD_Reference_ID,Value,AD_Client_ID,AD_Org_ID,IsActive,Created,CreatedBy,Updated,UpdatedBy,EntityType,AD_Ref_List_UU) VALUES (1122053,'MDF-e Autorizado com CT-e',1120220,'610614',0,0,'Y',TO_TIMESTAMP('2018-09-25 18:36:46','YYYY-MM-DD HH24:MI:SS'),100,TO_TIMESTAMP('2018-09-25 18:36:46','YYYY-MM-DD HH24:MI:SS'),100,'LBRA','583df27a-4bd5-47d1-bfe2-751f7d90818a')
+;
+
+-- 25/09/2018 18h36min56s BRT
+INSERT INTO AD_Ref_List (AD_Ref_List_ID,Name,AD_Reference_ID,Value,AD_Client_ID,AD_Org_ID,IsActive,Created,CreatedBy,Updated,UpdatedBy,EntityType,AD_Ref_List_UU) VALUES (1122054,'Cancelamento de MDF-e Autorizado com CT-e',1120220,'610615',0,0,'Y',TO_TIMESTAMP('2018-09-25 18:36:56','YYYY-MM-DD HH24:MI:SS'),100,TO_TIMESTAMP('2018-09-25 18:36:56','YYYY-MM-DD HH24:MI:SS'),100,'U','07933c8b-626c-4b9c-a051-50ff02c5f2ab')
+;
+
+-- 25/09/2018 18h37min6s BRT
+INSERT INTO AD_Ref_List (AD_Ref_List_ID,Name,AD_Reference_ID,Value,AD_Client_ID,AD_Org_ID,IsActive,Created,CreatedBy,Updated,UpdatedBy,EntityType,AD_Ref_List_UU) VALUES (1122055,'Averbação para Exportação',1120220,'790700',0,0,'Y',TO_TIMESTAMP('2018-09-25 18:37:06','YYYY-MM-DD HH24:MI:SS'),100,TO_TIMESTAMP('2018-09-25 18:37:06','YYYY-MM-DD HH24:MI:SS'),100,'U','c90d51dc-c771-4d8b-963e-91fb83d12fef')
+;
+
+-- 25/09/2018 18h37min15s BRT
+INSERT INTO AD_Ref_List (AD_Ref_List_ID,Name,AD_Reference_ID,Value,AD_Client_ID,AD_Org_ID,IsActive,Created,CreatedBy,Updated,UpdatedBy,EntityType,AD_Ref_List_UU) VALUES (1122056,'MDF-e Autorizado para CT-e',1120220,'310610',0,0,'Y',TO_TIMESTAMP('2018-09-25 18:37:15','YYYY-MM-DD HH24:MI:SS'),100,TO_TIMESTAMP('2018-09-25 18:37:15','YYYY-MM-DD HH24:MI:SS'),100,'U','57cb8cd8-369d-455d-912a-5f40b8161ef4')
+;
+
+-- 25/09/2018 18h37min26s BRT
+INSERT INTO AD_Ref_List (AD_Ref_List_ID,Name,AD_Reference_ID,Value,AD_Client_ID,AD_Org_ID,IsActive,Created,CreatedBy,Updated,UpdatedBy,EntityType,AD_Ref_List_UU) VALUES (1122057,'MDF-e Cancelado Vinculado a CT-e',1120220,'310611',0,0,'Y',TO_TIMESTAMP('2018-09-25 18:37:26','YYYY-MM-DD HH24:MI:SS'),100,TO_TIMESTAMP('2018-09-25 18:37:26','YYYY-MM-DD HH24:MI:SS'),100,'U','31285f0d-8e8b-4371-803a-954ca81c6c2f')
+;
+
+-- 25/09/2018 18h37min35s BRT
+INSERT INTO AD_Ref_List (AD_Ref_List_ID,Name,AD_Reference_ID,Value,AD_Client_ID,AD_Org_ID,IsActive,Created,CreatedBy,Updated,UpdatedBy,EntityType,AD_Ref_List_UU) VALUES (1122058,'Registro de Passagem de NFe propagado pelo MDFe',1120220,'610510',0,0,'Y',TO_TIMESTAMP('2018-09-25 18:37:35','YYYY-MM-DD HH24:MI:SS'),100,TO_TIMESTAMP('2018-09-25 18:37:35','YYYY-MM-DD HH24:MI:SS'),100,'U','88fdbb26-e967-4424-ace3-0c20abe46dcc')
+;
+
+-- 25/09/2018 18h37min47s BRT
+UPDATE AD_Ref_List SET EntityType='LBRA',Updated=TO_TIMESTAMP('2018-09-25 18:37:47','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Ref_List_ID=1122056
+;
+
+-- 25/09/2018 18h37min52s BRT
+UPDATE AD_Ref_List SET EntityType='LBRA',Updated=TO_TIMESTAMP('2018-09-25 18:37:52','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Ref_List_ID=1122057
+;
+
+-- 25/09/2018 18h38min5s BRT
+UPDATE AD_Ref_List SET EntityType='LBRA',Updated=TO_TIMESTAMP('2018-09-25 18:38:05','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Ref_List_ID=1122058
+;
+
+-- 25/09/2018 18h38min17s BRT
+UPDATE AD_Ref_List SET EntityType='LBRA',Updated=TO_TIMESTAMP('2018-09-25 18:38:17','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Ref_List_ID=1122054
+;
+
+-- 25/09/2018 18h38min23s BRT
+UPDATE AD_Ref_List SET EntityType='LBRA',Updated=TO_TIMESTAMP('2018-09-25 18:38:23','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Ref_List_ID=1122055
+;
+
+-- 25/09/2018 18h39min49s BRT
+INSERT INTO AD_Ref_List (AD_Ref_List_ID,Name,AD_Reference_ID,Value,AD_Client_ID,AD_Org_ID,IsActive,Created,CreatedBy,Updated,UpdatedBy,EntityType,AD_Ref_List_UU) VALUES (1122059,'Registro de Passagem de NFe propagado pelo MDFe/CTe',1120220,'610514',0,0,'Y',TO_TIMESTAMP('2018-09-25 18:39:49','YYYY-MM-DD HH24:MI:SS'),100,TO_TIMESTAMP('2018-09-25 18:39:49','YYYY-MM-DD HH24:MI:SS'),100,'LBRA','be9c49c2-c713-459a-85d8-33cf2b05b8cd')
+;
+
+SELECT Register_Migration_Script ('201809251800_NewTpEventoNFe.sql') FROM DUAL
+;
