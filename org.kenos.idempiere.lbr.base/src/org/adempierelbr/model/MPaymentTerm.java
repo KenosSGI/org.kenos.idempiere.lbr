a55f98736e7f639312702d2a10801c74dff61d92
Rogério Feitosa <rfeitosa@kenos.com.br>
2018-10-01 08:53:11 -0300

    Classe Alterada para permitir Gerar Condições de Pagamento para Fatura onde a Forma de Pagamento é diferente de Crédito

---
+++ org.kenos.idempiere.lbr.base/src/org/adempierelbr/model/MPaymentTerm.java
@@ -0,0 +1,271 @@
+/******************************************************************************
+ * Product: Adempiere ERP & CRM Smart Business Solution                       *
+ * Copyright (C) 1999-2006 ComPiere, Inc. All Rights Reserved.                *
+ * This program is free software; you can redistribute it and/or modify it    *
+ * under the terms version 2 of the GNU General Public License as published   *
+ * by the Free Software Foundation. This program is distributed in the hope   *
+ * that it will be useful, but WITHOUT ANY WARRANTY; without even the implied *
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.           *
+ * See the GNU General Public License for more details.                       *
+ * You should have received a copy of the GNU General Public License along    *
+ * with this program; if not, write to the Free Software Foundation, Inc.,    *
+ * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.                     *
+ * For the text or an alternative of this public license, you may reach us    *
+ * ComPiere, Inc., 2620 Augustine Dr. #245, Santa Clara, CA 95054, USA        *
+ * or via info@compiere.org or http://www.compiere.org/license.html           *
+ *****************************************************************************/
+package org.adempierelbr.model;
+
+import java.math.BigDecimal;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+import java.util.logging.Level;
+
+import org.adempiere.exceptions.AdempiereException;
+import org.compiere.model.I_C_InvoicePaySchedule;
+import org.compiere.model.MInvoice;
+import org.compiere.model.MInvoicePaySchedule;
+import org.compiere.model.MPaySchedule;
+import org.compiere.model.Query;
+import org.compiere.util.DB;
+import org.compiere.util.Env;
+import org.compiere.util.Msg;
+
+
+/**
+ * 
+ * @author kenos
+ *
+ */
+public class MPaymentTerm extends org.compiere.model.MPaymentTerm
+{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -4506224598566445450L;
+
+	/**
+	 * 	Standard Constructor
+	 *	@param ctx context
+	 *	@param C_PaymentTerm_ID id
+	 *	@param trxName transaction
+	 */
+	public MPaymentTerm(Properties ctx, int C_PaymentTerm_ID, String trxName)
+	{
+		super(ctx, C_PaymentTerm_ID, trxName);
+		
+	}	//	MPaymentTerm
+
+	/**
+	 * 	Load Constructor
+	 *	@param ctx context
+	 *	@param rs result set
+	 *	@param trxName transaction
+	 */
+	public MPaymentTerm(Properties ctx, ResultSet rs, String trxName)
+	{
+		super(ctx, rs, trxName);
+	}	//	MPaymentTerm
+
+	/**	Payment Schedule children			*/
+	private MPaySchedule[]				m_schedule;
+	
+	/**
+	 * 	Get Payment Schedule
+	 * 	@param requery if true re-query
+	 *	@return array of schedule
+	 */
+	public MPaySchedule[] getSchedule (boolean requery)
+	{
+		if (m_schedule != null && !requery)
+			return m_schedule;
+		String sql = "SELECT * FROM C_PaySchedule WHERE C_PaymentTerm_ID=? AND IsActive='Y' ORDER BY NetDays";
+		ArrayList<MPaySchedule> list = new ArrayList<MPaySchedule>();
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		try
+		{
+			pstmt = DB.prepareStatement(sql, get_TrxName());
+			pstmt.setInt(1, getC_PaymentTerm_ID());
+			rs = pstmt.executeQuery();
+			while (rs.next())
+			{
+				MPaySchedule ps = new MPaySchedule(getCtx(), rs, get_TrxName());
+				ps.setParent(this);
+				list.add (ps);
+			}
+		}
+		catch (Exception e)
+		{
+			log.log(Level.SEVERE, "getSchedule", e); 
+		}
+		finally
+		{
+			DB.close(rs, pstmt);
+			rs = null;
+			pstmt = null;
+		}
+		
+		m_schedule = new MPaySchedule[list.size()];
+		list.toArray(m_schedule);
+		return m_schedule;
+	}	//	getSchedule
+
+	/*************************************************************************
+	 * 	Apply Payment Term to Invoice -
+	 *	@param C_Invoice_ID invoice
+	 *	@return true if payment schedule is valid
+	 */
+	public boolean apply (int C_Invoice_ID)
+	{
+		MInvoice invoice = new MInvoice (getCtx(), C_Invoice_ID, get_TrxName());
+		if (invoice == null || invoice.get_ID() == 0)
+		{
+			log.log(Level.SEVERE, "apply - Not valid C_Invoice_ID=" + C_Invoice_ID);
+			return false;
+		}
+		return apply (invoice);
+	}	//	apply
+	
+	/**
+	 * 	Apply Payment Term to Invoice
+	 *	@param invoice invoice
+	 *	@return true if payment schedule is valid
+	 */
+	public boolean apply (MInvoice invoice)
+	{
+		if (invoice == null || invoice.get_ID() == 0)
+		{
+			log.log(Level.SEVERE, "No valid invoice - " + invoice);
+			return false;
+		}
+
+		// do not apply payment term if the invoice is not on credit or if total is zero
+		if (invoice.getGrandTotal().signum() == 0)
+			return false;
+			
+		if (!isValid())
+			return applyNoSchedule (invoice);
+		//
+		getSchedule(true);
+		if (m_schedule.length <= 0) // Allow schedules with just one record
+			return applyNoSchedule (invoice);
+		else	//	only if valid
+			return applySchedule(invoice);		
+	}	//	apply
+
+	/**
+	 * 	Apply Payment Term without schedule to Invoice
+	 *	@param invoice invoice
+	 *	@return false as no payment schedule
+	 */
+	private boolean applyNoSchedule (MInvoice invoice)
+	{
+		deleteInvoicePaySchedule (invoice.getC_Invoice_ID(), invoice.get_TrxName());
+		//	updateInvoice
+		if (invoice.getC_PaymentTerm_ID() != getC_PaymentTerm_ID())
+			invoice.setC_PaymentTerm_ID(getC_PaymentTerm_ID());
+		if (invoice.isPayScheduleValid())
+			invoice.setIsPayScheduleValid(false);
+		return false;
+	}	//	applyNoSchedule
+
+	/**
+	 * 	Apply Payment Term with schedule to Invoice
+	 *	@param invoice invoice
+	 *	@return true if payment schedule is valid
+	 */
+	private boolean applySchedule (MInvoice invoice)
+	{
+		deleteInvoicePaySchedule (invoice.getC_Invoice_ID(), invoice.get_TrxName());
+		//	Create Schedule
+		MInvoicePaySchedule ips = null;
+		BigDecimal remainder = invoice.getGrandTotal();
+		for (int i = 0; i < m_schedule.length; i++)
+		{
+			ips = new MInvoicePaySchedule (invoice, m_schedule[i]);
+			ips.saveEx(invoice.get_TrxName());
+			if (log.isLoggable(Level.FINE)) log.fine(ips.toString());
+			remainder = remainder.subtract(ips.getDueAmt());
+		}	//	for all schedules
+		//	Remainder - update last
+		if (remainder.compareTo(Env.ZERO) != 0 && ips != null)
+		{
+			ips.setDueAmt(ips.getDueAmt().add(remainder));
+			ips.saveEx(invoice.get_TrxName());
+			if (log.isLoggable(Level.FINE)) log.fine("Remainder=" + remainder + " - " + ips);
+		}
+		
+		//	updateInvoice
+		if (invoice.getC_PaymentTerm_ID() != getC_PaymentTerm_ID())
+			invoice.setC_PaymentTerm_ID(getC_PaymentTerm_ID());
+		return invoice.validatePaySchedule();
+	}	//	applySchedule
+
+	/**
+	 * 	Delete existing Invoice Payment Schedule
+	 *	@param C_Invoice_ID id
+	 *	@param trxName transaction
+	 */
+	private void deleteInvoicePaySchedule (int C_Invoice_ID, String trxName)
+	{
+		Query query = new Query(Env.getCtx(), I_C_InvoicePaySchedule.Table_Name, "C_Invoice_ID=?", trxName);
+		List<MInvoicePaySchedule> ipsList = query.setParameters(C_Invoice_ID).list();
+		for (MInvoicePaySchedule ips : ipsList)
+		{
+			ips.deleteEx(true);
+		}
+		if (log.isLoggable(Level.FINE)) log.fine("C_Invoice_ID=" + C_Invoice_ID + " - #" + ipsList.size());
+	}	//	deleteInvoicePaySchedule
+	
+	/**************************************************************************
+	 * 	String Representation
+	 *	@return info
+	 */
+	public String toString ()
+	{
+		StringBuilder sb = new StringBuilder ("MPaymentTerm[");
+		sb.append(get_ID()).append("-").append(getName())
+			.append(",Valid=").append(isValid())
+			.append ("]");
+		return sb.toString ();
+	}	//	toString
+	
+	/**
+	 * 	Before Save
+	 *	@param newRecord new
+	 *	@return true
+	 */
+	protected boolean beforeSave (boolean newRecord)
+	{
+		if (isDueFixed())
+		{
+			int dd = getFixMonthDay();
+			if (dd < 1 || dd > 31)
+			{
+				log.saveError("Error", Msg.parseTranslation(getCtx(), "@Invalid@ @FixMonthDay@"));
+				return false;
+			}
+			dd = getFixMonthCutoff();
+			if (dd < 1 || dd > 31)
+			{
+				log.saveError("Error", Msg.parseTranslation(getCtx(), "@Invalid@ @FixMonthCutoff@"));
+				return false;
+			}
+		}
+		
+		if (Integer.signum(getNetDays()) < 0)
+		{
+			throw new AdempiereException(Msg.parseTranslation(getCtx(), "@NetDays@") + " " +
+										 Msg.parseTranslation(getCtx(), "@positive.number@"));
+		}
+		
+		if (!newRecord || !isValid())
+			validate();
+		return true;
+	}	//	beforeSave
+	
+}	//	MPaymentTerm
