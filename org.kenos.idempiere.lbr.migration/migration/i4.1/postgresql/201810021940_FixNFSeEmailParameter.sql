da0d12fc7d537fbd9dadd473ebe963092b2eabed
Ricardo Santana <rsantana@kenos.com.br>
2018-10-02 19:45:08 -0300

    Fix NFSe E-mail Parameter

---
+++ org.kenos.idempiere.lbr.migration/migration/i4.1/postgresql/201810021940_FixNFSeEmailParameter.sql
@@ -0,0 +1,6 @@
+-- 02/10/2018 19h39min6s BRT
+UPDATE AD_Process_Para SET DefaultValue='@SQL=SELECT (CASE WHEN ''@lbr_NFModel@''=''S1'' THEN COALESCE (LBR_EMailNFSe,lbr_EMailNFe) ELSE COALESCE (lbr_EMailNFe,lbr_EMailNFe) END) FROM C_BPartner WHERE C_BPartner_ID=@C_BPartner_ID@',Updated=TO_TIMESTAMP('2018-10-02 19:39:06','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Process_Para_ID=1120237
+;
+
+SELECT Register_Migration_Script ('201810021940_FixNFSeEmailParameter.sql') FROM DUAL
+;
