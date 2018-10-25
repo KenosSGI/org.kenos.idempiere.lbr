b4195023975d6ef648c6a95d29f3dd5b7d62ebb8
Rogério Feitosa <rfeitosa@kenos.com.br>
2018-10-25 18:08:25 -0200

    Campo Localização do Parceiro de Negócio que possuir Filtro sendo alterado de Pesquisa para Tabela Direta.

---
+++ org.kenos.idempiere.lbr.migration/migration/i4.1/oracle/201810251800_BPartnerLocationToDirectTable.sql
@@ -0,0 +1,9 @@
+SET SQLBLANKLINES ON
+SET DEFINE OFF
+
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+-- 25/10/2018 17h50min0s BRT
+UPDATE AD_Column SET AD_Reference_ID=19 WHERE ColumnName='C_BPartner_Location_ID' AND AD_Reference_ID=30 AND AD_Val_Rule_ID IS NOT NULL;
+
+SELECT Register_Migration_Script ('201810251800_BPartnerLocationToDirectTable.sql') FROM DUAL
+;
