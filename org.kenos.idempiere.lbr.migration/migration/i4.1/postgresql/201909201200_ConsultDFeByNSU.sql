0ed1ee302a48fcece8257ad9e5f2abb8e22158d2
Rogério Feitosa <rfeitosa@kenos.com.br>
2019-09-20 12:08:41 -0300

    Registra os NSU dos Documentos Fiscais.

---
+++ org.kenos.idempiere.lbr.migration/migration/i4.1/postgresql/201909201200_ConsultDFeByNSU.sql
@@ -0,0 +1,77 @@
+-- 18/09/2019 12h37min11s BRT
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+INSERT INTO AD_Element (AD_Client_ID,AD_Element_ID,AD_Org_ID,ColumnName,Created,CreatedBy,Description,EntityType,Help,IsActive,Name,PrintName,Updated,UpdatedBy) VALUES (0,1122885,0,'LBR_NSU',TO_TIMESTAMP('2019-09-18 12:37:10','YYYY-MM-DD HH24:MI:SS'),100,'Unic Sequencial Number used on Manifest of Fiscal Documento','LBRA','Unic Sequencial Number used on Manifest of Fiscal Documento','Y','NSU','NSU',TO_TIMESTAMP('2019-09-18 12:37:10','YYYY-MM-DD HH24:MI:SS'),100)
+;
+
+-- 18/09/2019 12h37min11s BRT
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+INSERT INTO AD_Element_Trl (AD_Language,AD_Element_ID, Description,Help,Name,PO_Description,PO_Help,PO_Name,PO_PrintName,PrintName, IsTranslated,AD_Client_ID,AD_Org_ID,Created,Createdby,Updated,UpdatedBy) SELECT l.AD_Language,t.AD_Element_ID, t.Description,t.Help,t.Name,t.PO_Description,t.PO_Help,t.PO_Name,t.PO_PrintName,t.PrintName, 'N',t.AD_Client_ID,t.AD_Org_ID,t.Created,t.Createdby,t.Updated,t.UpdatedBy FROM AD_Language l, AD_Element t WHERE l.IsActive='Y' AND l.IsSystemLanguage='Y' AND l.IsBaseLanguage='N' AND t.AD_Element_ID=1122885 AND NOT EXISTS (SELECT * FROM AD_Element_Trl tt WHERE tt.AD_Language=l.AD_Language AND tt.AD_Element_ID=t.AD_Element_ID)
+;
+
+-- 18/09/2019 12h37min21s BRT
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Element_Trl SET IsTranslated='Y',Updated=TO_TIMESTAMP('2019-09-18 12:37:21','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Element_ID=1122885 AND AD_Language='pt_BR'
+;
+
+-- 18/09/2019 12h38min30s BRT
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Element_Trl SET Description='Número Sequencial Único gerado na Manifestação de Documento Fiscal',Help='Número Sequencial Único gerado na Manifestação de Documento Fiscal',Updated=TO_TIMESTAMP('2019-09-18 12:38:30','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Element_ID=1122885 AND AD_Language='pt_BR'
+;
+
+-- 18/09/2019 15h43min29s BRT
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+INSERT INTO AD_Column (AD_Client_ID,AD_Column_ID,AD_Element_ID,AD_Org_ID,AD_Reference_ID,AD_Table_ID,ColumnName,Created,CreatedBy,Description,EntityType,FieldLength,Help,IsActive,IsAllowLogging,IsAlwaysUpdateable,IsAutocomplete,IsEncrypted,IsIdentifier,IsKey,IsMandatory,IsParent,IsSelectionColumn,IsSyncDatabase,IsTranslated,IsUpdateable,Name,SeqNo,Updated,UpdatedBy,Version) VALUES (0,1131527,1122885,0,10,1120461,'LBR_NSU',TO_TIMESTAMP('2019-09-18 15:43:28','YYYY-MM-DD HH24:MI:SS'),100,'Unic Sequencial Number used on Manifest of Fiscal Documento','LBRA',60,'Unic Sequencial Number used on Manifest of Fiscal Documento','Y','Y','N','N','N','N','N','N','N','N','N','N','Y','NSU',0,TO_TIMESTAMP('2019-09-18 15:43:28','YYYY-MM-DD HH24:MI:SS'),100,0)
+;
+
+-- 18/09/2019 15h43min29s BRT
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+INSERT INTO AD_Column_Trl (AD_Language,AD_Column_ID, Name, IsTranslated,AD_Client_ID,AD_Org_ID,Created,Createdby,Updated,UpdatedBy) SELECT l.AD_Language,t.AD_Column_ID, t.Name, 'N',t.AD_Client_ID,t.AD_Org_ID,t.Created,t.Createdby,t.Updated,t.UpdatedBy FROM AD_Language l, AD_Column t WHERE l.IsActive='Y' AND l.IsSystemLanguage='Y' AND l.IsBaseLanguage='N' AND t.AD_Column_ID=1131527 AND NOT EXISTS (SELECT * FROM AD_Column_Trl tt WHERE tt.AD_Language=l.AD_Language AND tt.AD_Column_ID=t.AD_Column_ID)
+;
+
+-- 18/09/2019 15h43min46s BRT
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+ALTER TABLE LBR_PartnerDFe ADD COLUMN LBR_NSU VARCHAR(60) DEFAULT NULL 
+;
+
+-- 18/09/2019 15h45min11s BRT
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+INSERT INTO t_alter_column values('lbr_partnerdfe','LBR_NSU','VARCHAR(60)',null,'NULL')
+;
+
+-- 18/09/2019 15h45min37s BRT
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+INSERT INTO AD_Field (AD_Client_ID,AD_Column_ID,AD_Field_ID,AD_Org_ID,AD_Tab_ID,Created,CreatedBy,Description,DisplayLength,EntityType,Help,IsActive,IsCentrallyMaintained,IsDisplayed,IsEncrypted,IsFieldOnly,IsHeading,IsReadOnly,IsSameLine,Name,SeqNo,SortNo,Updated,UpdatedBy) VALUES (0,1131527,1128205,0,1120197,TO_TIMESTAMP('2019-09-18 15:45:37','YYYY-MM-DD HH24:MI:SS'),100,'Unic Sequencial Number used on Manifest of Fiscal Documento',10,'LBRA','Unic Sequencial Number used on Manifest of Fiscal Documento','Y','Y','Y','N','N','N','N','N','NSU',210,0,TO_TIMESTAMP('2019-09-18 15:45:37','YYYY-MM-DD HH24:MI:SS'),100)
+;
+
+-- 18/09/2019 15h45min37s BRT
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+INSERT INTO AD_Field_Trl (AD_Language,AD_Field_ID, Description,Help,Name, IsTranslated,AD_Client_ID,AD_Org_ID,Created,Createdby,Updated,UpdatedBy) SELECT l.AD_Language,t.AD_Field_ID, t.Description,t.Help,t.Name, 'N',t.AD_Client_ID,t.AD_Org_ID,t.Created,t.Createdby,t.Updated,t.UpdatedBy FROM AD_Language l, AD_Field t WHERE l.IsActive='Y' AND l.IsSystemLanguage='Y' AND l.IsBaseLanguage='N' AND t.AD_Field_ID=1128205 AND NOT EXISTS (SELECT * FROM AD_Field_Trl tt WHERE tt.AD_Language=l.AD_Language AND tt.AD_Field_ID=t.AD_Field_ID)
+;
+
+-- 19/09/2019 14h37min29s BRT
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+INSERT INTO AD_Process_Para (AD_Client_ID,AD_Element_ID,AD_Org_ID,AD_Process_ID,AD_Process_Para_ID,AD_Reference_ID,ColumnName,Created,CreatedBy,EntityType,FieldLength,IsActive,IsCentrallyMaintained,IsMandatory,IsRange,Name,SeqNo,Updated,UpdatedBy) VALUES (0,1122885,0,1120159,1120479,10,'LBR_NSU',TO_TIMESTAMP('2019-09-19 14:37:28','YYYY-MM-DD HH24:MI:SS'),100,'LBRA',60,'Y','Y','N','N','NSU',20,TO_TIMESTAMP('2019-09-19 14:37:28','YYYY-MM-DD HH24:MI:SS'),100)
+;
+
+-- 19/09/2019 14h37min29s BRT
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+INSERT INTO AD_Process_Para_Trl (AD_Language,AD_Process_Para_ID, Description,Help,Name, IsTranslated,AD_Client_ID,AD_Org_ID,Created,Createdby,Updated,UpdatedBy) SELECT l.AD_Language,t.AD_Process_Para_ID, t.Description,t.Help,t.Name, 'N',t.AD_Client_ID,t.AD_Org_ID,t.Created,t.Createdby,t.Updated,t.UpdatedBy FROM AD_Language l, AD_Process_Para t WHERE l.IsActive='Y' AND l.IsSystemLanguage='Y' AND l.IsBaseLanguage='N' AND t.AD_Process_Para_ID=1120479 AND NOT EXISTS (SELECT * FROM AD_Process_Para_Trl tt WHERE tt.AD_Language=l.AD_Language AND tt.AD_Process_Para_ID=t.AD_Process_Para_ID)
+;
+
+-- 19/09/2019 18h24min3s BRT
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+INSERT INTO AD_Field (AD_Client_ID,AD_Column_ID,AD_Field_ID,AD_Org_ID,AD_Tab_ID,Created,CreatedBy,Description,DisplayLength,EntityType,Help,IsActive,IsCentrallyMaintained,IsDisplayed,IsEncrypted,IsFieldOnly,IsHeading,IsReadOnly,IsSameLine,Name,SeqNo,SortNo,Updated,UpdatedBy) VALUES (0,1131527,1128206,0,1120198,TO_TIMESTAMP('2019-09-19 18:24:03','YYYY-MM-DD HH24:MI:SS'),100,'Unic Sequencial Number used on Manifest of Fiscal Documento',10,'LBRA','Unic Sequencial Number used on Manifest of Fiscal Documento','Y','Y','Y','N','N','N','N','N','NSU',150,0,TO_TIMESTAMP('2019-09-19 18:24:03','YYYY-MM-DD HH24:MI:SS'),100)
+;
+
+-- 19/09/2019 18h24min3s BRT
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+INSERT INTO AD_Field_Trl (AD_Language,AD_Field_ID, Description,Help,Name, IsTranslated,AD_Client_ID,AD_Org_ID,Created,Createdby,Updated,UpdatedBy) SELECT l.AD_Language,t.AD_Field_ID, t.Description,t.Help,t.Name, 'N',t.AD_Client_ID,t.AD_Org_ID,t.Created,t.Createdby,t.Updated,t.UpdatedBy FROM AD_Language l, AD_Field t WHERE l.IsActive='Y' AND l.IsSystemLanguage='Y' AND l.IsBaseLanguage='N' AND t.AD_Field_ID=1128206 AND NOT EXISTS (SELECT * FROM AD_Field_Trl tt WHERE tt.AD_Language=l.AD_Language AND tt.AD_Field_ID=t.AD_Field_ID)
+;
+
+-- 19/09/2019 18h24min37s BRT
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Field SET IsSameLine='Y', SeqNo=160,Updated=TO_TIMESTAMP('2019-09-19 18:24:37','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=1125536
+;
+
+SELECT Register_Migration_Script ('201909201200_ConsultDFeByNSU.sql') FROM DUAL
+;
