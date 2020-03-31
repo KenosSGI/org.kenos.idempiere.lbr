a19f341ebc609203fc9747d9d7da2559c4e08419
Rogério Feitosa <rfeitosa@kenos.com.br>
2020-03-31 15:36:51 -0300

    Ajuste do Script adicionando linha Register Migration Script

--- org.kenos.idempiere.lbr.migration/migration/i6.2/postgresql/202003271630_CreateProductionFromOrderLBR.sql
+++ org.kenos.idempiere.lbr.migration/migration/i6.2/postgresql/202003271630_CreateProductionFromOrderLBR.sql
@@ -3,3 +3,5 @@
 UPDATE AD_Process SET Classname='org.kenos.idempiere.lbr.base.process.OrderLineCreateProduction',Updated=TO_TIMESTAMP('2020-03-27 15:58:48','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Process_ID=200006
 ;
 
+SELECT Register_Migration_Script ('202003271630_CreateProductionFromOrderLBR.sql') FROM DUAL
+;
