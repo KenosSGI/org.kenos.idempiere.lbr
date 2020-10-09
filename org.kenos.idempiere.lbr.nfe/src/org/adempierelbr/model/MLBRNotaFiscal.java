3b27606dc9a782f5b1a2dc8fcfbf5e90aeb438b0
Rogério Feitosa <rfeitosa@kenos.com.br>
2020-10-09 10:44:31 -0300

    Corrigido erro ao Gerar NF a partir de um Pedido ou Outras Entradas/Saída

--- org.kenos.idempiere.lbr.nfe/src/org/adempierelbr/model/MLBRNotaFiscal.java
+++ org.kenos.idempiere.lbr.nfe/src/org/adempierelbr/model/MLBRNotaFiscal.java
@@ -1905,7 +1905,9 @@ public class MLBRNotaFiscal extends X_LBR_NotaFiscal implements DocAction, DocOp
 				continue;
 			
 			MLBRNotaFiscalLine nfLine = new MLBRNotaFiscalLine (this);
+			nfLine.setAD_Org_ID(getAD_Org_ID());
 			nfLine.save();
+			//
 			nfLine.setLine(lineNo++);
 			nfLine.setOrderLine(oLine, false);
 			nfLine.save();
