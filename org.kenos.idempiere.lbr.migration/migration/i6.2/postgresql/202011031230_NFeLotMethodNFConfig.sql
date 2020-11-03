beaea4c965763f99cc3debbe0a9107c14f3b5388
Rogério Feitosa <rfeitosa@kenos.com.br>
2020-11-03 12:47:02 -0300

    Fix Script

--- org.kenos.idempiere.lbr.migration/migration/i6.2/postgresql/202011031230_NFeLotMethodNFConfig.sql
+++ org.kenos.idempiere.lbr.migration/migration/i6.2/postgresql/202011031230_NFeLotMethodNFConfig.sql
@@ -3,8 +3,13 @@
 INSERT INTO AD_Column (AD_Column_ID,Version,Name,Description,AD_Table_ID,ColumnName,DefaultValue,FieldLength,IsKey,IsParent,IsMandatory,IsTranslated,IsIdentifier,SeqNo,IsEncrypted,AD_Reference_ID,AD_Reference_Value_ID,AD_Client_ID,AD_Org_ID,IsActive,Created,CreatedBy,Updated,UpdatedBy,AD_Element_ID,IsUpdateable,IsSelectionColumn,ReadOnlyLogic,EntityType,IsSyncDatabase,IsAlwaysUpdateable,IsAutocomplete,IsAllowLogging,AD_Column_UU,IsAllowCopy,SeqNoSelection,IsToolbarButton,IsSecure,IsHtml) VALUES (1132766,0,'Lot Method','Method of transmission of NFe Lot',1120547,'LBR_NFeLotMethod','0',1,'N','N','N','N','N',0,'N',17,1120209,0,0,'Y',TO_TIMESTAMP('2020-11-03 11:50:02','YYYY-MM-DD HH24:MI:SS'),100,TO_TIMESTAMP('2020-11-03 11:50:02','YYYY-MM-DD HH24:MI:SS'),100,1121724,'Y','N','@DocStatus@!DR','LBRA','N','N','N','Y','84f4b8d2-8c40-4e83-8365-5f158f2ff81a','Y',0,'N','N','N')
 ;
 
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+-- 3 de nov de 2020 12:37:24 BRT
+UPDATE AD_Column SET DefaultValue=NULL, ReadOnlyLogic=NULL,Updated=TO_TIMESTAMP('2020-11-03 12:37:24','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=1132766
+;
+
 -- 3 de nov de 2020 11:50:13 BRT
-ALTER TABLE LBR_NFConfig ADD COLUMN LBR_NFeLotMethod CHAR(1) DEFAULT '0' 
+ALTER TABLE LBR_NFConfig ADD COLUMN LBR_NFeLotMethod CHAR(1) DEFAULT NULL 
 ;
 
 -- 3 de nov de 2020 11:51:21 BRT
