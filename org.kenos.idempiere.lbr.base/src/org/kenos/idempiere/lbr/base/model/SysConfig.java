f37306a950a48f913cf6718c1aebfd489a8ea2c8
Ricardo Santana <rsantana@kenos.com.br>
2020-11-13 12:21:55 -0300

    NFSe atibaia

--- org.kenos.idempiere.lbr.base/src/org/kenos/idempiere/lbr/base/model/SysConfig.java
+++ org.kenos.idempiere.lbr.base/src/org/kenos/idempiere/lbr/base/model/SysConfig.java
@@ -51,6 +51,7 @@ public class SysConfig
 	public static final String LBR_NFSE_PALHOCA_PASS 							= "LBR_NFSE_PALHOCA_PASS";
 	public static final String LBR_NFSE_PALHOCA_USER 							= "LBR_NFSE_PALHOCA_USER";
 	public static final String LBR_NFSE_SP_PRINT_URL 							= "LBR_NFSE_SP_PRINT_URL";
+	public static final String LBR_NFSE_TOKEN 									= "LBR_NFSE_TOKEN";
 	public static final String LBR_OVERWRITE_ORDER_PAY_SCHEDULE					= "LBR_OVERWRITE_ORDER_PAY_SCHEDULE";
 	public static final String LBR_POG_INVOICE_COMPLETE 						= "LBR_POG_INVOICE_COMPLETE";
 	public static final String LBR_PRINT_BP_PRODUCT_NO_NF 						= "LBR_PRINT_BP_PRODUCT_NO_NF";
