175994fc9fd3324fd7050fda256db6b39aa9a335
Ricardo Santana <rsantana@kenos.com.br>
2021-02-19 17:39:53 -0300

    check city name

---
+++ org.kenos.idempiere.lbr.base/src/org/kenos/idempiere/lbr/base/model/MCity.java
@@ -0,0 +1,58 @@
+package org.kenos.idempiere.lbr.base.model;
+
+import java.sql.ResultSet;
+import java.util.Properties;
+
+import org.adempiere.model.POWrapper;
+import org.adempierelbr.wrapper.I_W_C_City;
+import org.compiere.model.Query;
+import org.compiere.util.Util;
+
+/**
+ * 		MCity
+ * 
+ * 	@author Ricardo Santana (Kenos, www.kenos.com.br)
+ *	@version $Id: MCity.java, v1.0 2021/02/19 10:25:35 AM, ralexsander Exp $
+ */
+public class MCity extends org.compiere.model.MCity
+{
+	/**
+	 * 	Serial ID
+	 */
+	private static final long serialVersionUID = 2312021924951710950L;
+	private I_W_C_City wrapper = null;
+
+	public MCity (Properties ctx, int C_City_ID, String trxName)
+	{
+		super(ctx, C_City_ID, trxName);
+		wrapper = POWrapper.create(this, I_W_C_City.class);
+	}	//	MCity
+	
+	public MCity (Properties ctx, ResultSet rs, String trxName)
+	{
+		super(ctx, rs, trxName);
+		wrapper = POWrapper.create(this, I_W_C_City.class);
+	}	//	MCity
+	
+	public I_W_C_City getWrapper ()
+	{
+		return wrapper;
+	}	//	getWrapper
+	
+	public static MCity getCity (Properties ctx, int C_Region_ID, String cityName)
+	{
+		if (cityName == null)
+			return null;
+		MCity city = null;
+		//
+		try
+		{
+			city = new Query (ctx, Table_Name, COLUMNNAME_C_Region_ID+"=? AND UNACCENT_UPPER("+MCity.COLUMNNAME_Name+")=?", null)
+				.setParameters(C_Region_ID, Util.deleteAccents(cityName.trim().toUpperCase().replaceAll("\\s+", " ")))
+				.setOnlyActiveRecords(true)
+				.firstOnly();
+		} 
+		catch (Exception e) {}
+		return city;
+	}	//	getCity
+}	//	MCity
