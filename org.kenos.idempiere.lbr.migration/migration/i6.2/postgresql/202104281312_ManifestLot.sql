bc11d51c882b54c002b2ec878123661adad410c4
Ricardo Santana <rsantana@kenos.com.br>
2021-04-28 13:31:41 -0300

    manifest multiple docs

---
+++ org.kenos.idempiere.lbr.migration/migration/i6.2/postgresql/202104281312_ManifestLot.sql
@@ -0,0 +1,47 @@
+-- 28 de abr de 2021 12:22:35 BRT
+INSERT INTO AD_Process_Para (AD_Process_Para_ID,AD_Client_ID,AD_Org_ID,IsActive,Created,CreatedBy,Updated,UpdatedBy,Name,Description,Help,AD_Process_ID,SeqNo,AD_Reference_ID,IsRange,AD_Val_Rule_ID,FieldLength,IsMandatory,ColumnName,IsCentrallyMaintained,EntityType,AD_Element_ID,AD_Process_Para_UU,IsEncrypted) VALUES (1120569,0,0,'Y',TO_TIMESTAMP('2021-04-28 12:22:35','YYYY-MM-DD HH24:MI:SS'),100,TO_TIMESTAMP('2021-04-28 12:22:35','YYYY-MM-DD HH24:MI:SS'),100,'Organization','Organizational entity within client','An organization is a unit of your client or legal entity - examples are store, department. You can share data between organizations.',1120161,5,17,'N',202,0,'N','AD_Org_ID','Y','LBRA',113,'d2f5ebdf-e6c9-475e-b474-f18c321f0928','N')
+;
+
+-- 28 de abr de 2021 12:23:18 BRT
+INSERT INTO AD_Process_Para (AD_Process_Para_ID,AD_Client_ID,AD_Org_ID,IsActive,Created,CreatedBy,Updated,UpdatedBy,Name,AD_Process_ID,SeqNo,AD_Reference_ID,AD_Reference_Value_ID,IsRange,FieldLength,IsMandatory,ColumnName,IsCentrallyMaintained,EntityType,AD_Element_ID,AD_Process_Para_UU,IsEncrypted) VALUES (1120570,0,0,'Y',TO_TIMESTAMP('2021-04-28 12:23:17','YYYY-MM-DD HH24:MI:SS'),100,TO_TIMESTAMP('2021-04-28 12:23:17','YYYY-MM-DD HH24:MI:SS'),100,'NFe Environment',1120161,30,17,1100001,'N',0,'N','lbr_NFeEnv','Y','LBRA',1100011,'a5bc73ac-1604-4854-bb82-268a43db544b','N')
+;
+
+-- 28 de abr de 2021 12:25:12 BRT
+INSERT INTO AD_Element (AD_Element_ID,AD_Client_ID,AD_Org_ID,IsActive,Created,CreatedBy,Updated,UpdatedBy,ColumnName,Name,Description,Help,PrintName,EntityType,AD_Element_UU) VALUES (1123386,0,0,'Y',TO_TIMESTAMP('2021-04-28 12:24:52','YYYY-MM-DD HH24:MI:SS'),100,TO_TIMESTAMP('2021-04-28 12:24:52','YYYY-MM-DD HH24:MI:SS'),100,'LBR_ManifestTries','Manifest Tries',NULL,NULL,'Manifest Tries','LBRA','412ab3ee-689b-42a8-9d02-340da5f8409f')
+;
+
+-- 28 de abr de 2021 12:25:29 BRT
+INSERT INTO AD_Column (AD_Column_ID,Version,Name,AD_Table_ID,ColumnName,FieldLength,IsKey,IsParent,IsMandatory,IsTranslated,IsIdentifier,SeqNo,IsEncrypted,AD_Reference_ID,AD_Client_ID,AD_Org_ID,IsActive,Created,CreatedBy,Updated,UpdatedBy,AD_Element_ID,IsUpdateable,IsSelectionColumn,EntityType,IsSyncDatabase,IsAlwaysUpdateable,IsAutocomplete,IsAllowLogging,AD_Column_UU,IsAllowCopy,SeqNoSelection,IsToolbarButton,IsSecure,FKConstraintType,IsHtml) VALUES (1133729,0,'Manifest Tries',1120461,'LBR_ManifestTries',1,'N','N','N','N','N',0,'N',11,0,0,'Y',TO_TIMESTAMP('2021-04-28 12:25:29','YYYY-MM-DD HH24:MI:SS'),100,TO_TIMESTAMP('2021-04-28 12:25:29','YYYY-MM-DD HH24:MI:SS'),100,1123386,'Y','N','LBRA','N','N','N','Y','82988561-08f5-4e63-9f8f-69d7cc5d52b8','Y',0,'N','N','N','N')
+;
+
+-- 28 de abr de 2021 12:25:59 BRT
+UPDATE AD_Column SET IsAllowLogging='N', IsAllowCopy='N',Updated=TO_TIMESTAMP('2021-04-28 12:25:59','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=1133729
+;
+
+-- 28 de abr de 2021 12:26:02 BRT
+ALTER TABLE LBR_PartnerDFe ADD COLUMN LBR_ManifestTries NUMERIC(10) DEFAULT NULL 
+;
+
+-- 28 de abr de 2021 13:19:54 BRT
+UPDATE AD_Process_Para SET AD_Val_Rule_ID=130,Updated=TO_TIMESTAMP('2021-04-28 13:19:54','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Process_Para_ID=1120569
+;
+
+-- 28 de abr de 2021 13:20:18 BRT
+UPDATE AD_Process_Para SET DisplayLogic='@LBR_PartnerDFe_ID@!0',Updated=TO_TIMESTAMP('2021-04-28 13:20:18','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Process_Para_ID=1120569
+;
+
+-- 28 de abr de 2021 13:20:24 BRT
+UPDATE AD_Process_Para SET DisplayLogic='@LBR_PartnerDFe_ID@!0',Updated=TO_TIMESTAMP('2021-04-28 13:20:24','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Process_Para_ID=1120570
+;
+
+-- 28 de abr de 2021 13:25:14 BRT
+UPDATE AD_Process_Para SET AD_Reference_ID=19, DisplayLogic='@LBR_PartnerDFe_ID@=0',Updated=TO_TIMESTAMP('2021-04-28 13:25:14','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Process_Para_ID=1120569
+;
+
+-- 28 de abr de 2021 13:25:17 BRT
+UPDATE AD_Process_Para SET DisplayLogic='@LBR_PartnerDFe_ID@=0',Updated=TO_TIMESTAMP('2021-04-28 13:25:17','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Process_Para_ID=1120570
+;
+
+-- 28 de abr de 2021 12:26:02 BRT
+SELECT Register_Migration_Script ('202104281312_ManifestLot.sql') FROM DUAL
+;
--- org.kenos.idempiere.lbr.nfe/src/org/adempierelbr/process/ManifestDFe.java
+++ org.kenos.idempiere.lbr.nfe/src/org/adempierelbr/process/ManifestDFe.java
@@ -1,16 +1,23 @@
 package org.adempierelbr.process;
 
 import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.logging.Level;
 
-import org.adempiere.exceptions.AdempiereException;
 import org.adempierelbr.model.MLBRNFeEvent;
+import org.adempierelbr.model.MLBRNotaFiscal;
 import org.adempierelbr.model.MLBRPartnerDFe;
+import org.compiere.model.MSysConfig;
+import org.compiere.model.Query;
 import org.compiere.process.ProcessInfoParameter;
 import org.compiere.process.SvrProcess;
 import org.compiere.util.AdempiereUserError;
 import org.compiere.util.CLogger;
+import org.compiere.util.DB;
 import org.compiere.util.Env;
+import org.compiere.util.TimeUtil;
+import org.kenos.idempiere.lbr.base.model.SysConfig;
 
 /**
  * 		Processo para manifestar a NF emitida para a sua Organização
@@ -21,13 +28,19 @@ import org.compiere.util.Env;
 public class ManifestDFe extends SvrProcess
 {
 	/**	Record ID				*/
-	private int p_Record_ID = -1;
+	protected int p_Record_ID = -1;
 	
 	/**	CPF							*/
-	public String p_EventType = null;
+	protected String p_EventType = null;
 	
 	/**	CNPJ						*/
-	public String p_XJust = null;
+	protected String p_XJust = null;
+	
+	/**	Environment					*/
+	protected String p_LBR_NFeEnv = null;
+	
+	/**	Organization				*/
+	protected int p_AD_Org_ID = -1;
 	
 	/** Log							*/
 	private static CLogger log = CLogger.getCLogger (ManifestDFe.class);
@@ -50,6 +63,12 @@ public class ManifestDFe extends SvrProcess
 			else if ("LBR_EventType".equals (name))
 				p_EventType = (String) para[i].getParameter();
 			
+			else if (MLBRNotaFiscal.COLUMNNAME_lbr_NFeEnv.equals (name))
+				p_LBR_NFeEnv = (String) para[i].getParameter();
+			
+			else if (MLBRNFeEvent.COLUMNNAME_AD_Org_ID.equals (name))
+				p_AD_Org_ID = para[i].getParameterAsInt();
+			
 			else
 				log.log(Level.SEVERE, "Unknown Parameter: " + name);
 		}
@@ -66,24 +85,59 @@ public class ManifestDFe extends SvrProcess
 	protected String doIt () throws Exception
 	{
 		//	Organization
-		if (p_Record_ID <= 0)
-			throw new AdempiereUserError ("@FillMandatory@  @Record_ID@");
+		if (p_Record_ID < 1 && p_AD_Org_ID < 1)
+			throw new AdempiereUserError ("@FillMandatory@  @Record_ID@ / @AD_Org_ID@");
 		
 		//	Unified Business Partner
 		if (p_EventType == null || p_EventType.isEmpty())
 			throw new AdempiereUserError ("@FillMandatory@  @LBR_EventType@");
+				
+		List<MLBRPartnerDFe> dfes = new ArrayList<MLBRPartnerDFe>();
 		
-		//	DF-e
-		MLBRPartnerDFe dfe = new MLBRPartnerDFe (Env.getCtx(), p_Record_ID, get_TrxName());
+		//	Single DF-e
+		if (p_Record_ID > 0)
+			dfes.add(new MLBRPartnerDFe (Env.getCtx(), p_Record_ID, get_TrxName()));
 		
+		//	All available, only to manifest Ciência da Operação
+		else if (MLBRPartnerDFe.LBR_EVENTTYPE_CienciaDaOperacao.equals(p_EventType))
+		{
+			Timestamp today = new Timestamp (System.currentTimeMillis());
+			//
+			String whereClause =  "AD_Org_ID=? "	//	Same Org
+					+ " AND LBR_IsXMLValid='N' "	//	Not Downloaded Yet
+					+ " AND DocumentType='0' "		//	Only NF-e (Exclude Events)
+					+ " AND IsCancelled='N' "		//	Not Cancelled
+					+ " AND LBR_IsManifested='N' "	//	Only Manifested DF-e
+					+ " AND LBR_ManifestTries IS NULL OR LBR_ManifestTries<" + MSysConfig.getIntValue(SysConfig.LBR_MANIFEST_TRIES, 5)
+					+ " AND DateDoc >= " + DB.TO_DATE (TimeUtil.addDays (today, -180));	//	180 days is the limit to manifest a document
+			// 
+			dfes = new Query(Env.getCtx(), MLBRPartnerDFe.Table_Name, whereClause, get_TrxName())
+									.setParameters(p_AD_Org_ID)
+									.list();
+		}
+		
+		//	Manifest all DF-es
+		dfes.stream().forEach(this::manifest);
+		
+		return "@Success@";
+	}	//	doIt
+
+	/**
+	 * 	Manifest Doc Fiscal
+	 * 	@param dfe
+	 */
+	protected void manifest (MLBRPartnerDFe dfe) 
+	{		
 		//	Event
 		MLBRNFeEvent event = new MLBRNFeEvent (getCtx(), 0, get_TrxName());
 		event.setAD_Org_ID(dfe.getAD_Org_ID());
-		event.setLBR_PartnerDFe_ID(p_Record_ID);
+		event.setLBR_PartnerDFe_ID(dfe.getLBR_PartnerDFe_ID());
 		event.setlbr_NFeID(dfe.getlbr_NFeID());
 		event.setLBR_EventType(p_EventType);
-		event.setlbr_NFModel(MLBRNFeEvent.LBR_NFMODEL_NotaFiscalEletrônica);	//	FIXME: Verificar se é necessário
+		event.setlbr_NFModel(MLBRNFeEvent.LBR_NFMODEL_NotaFiscalEletrônica);
 		event.setProtocol(dfe.getlbr_NFeProt());
+		if (p_LBR_NFeEnv != null)
+			event.setlbr_NFeEnv(p_LBR_NFeEnv);
 		
 		//	Only for 210240
 		if (MLBRNFeEvent.LBR_EVENTTYPE_OperacaoNaoRealizada.equals(p_EventType))
@@ -109,9 +163,10 @@ public class ManifestDFe extends SvrProcess
 		}
 		else
 		{
-			throw new AdempiereException ("Falha ao completar o registro do evento. " + event.getProcessMsg());
+			dfe.increaseTries();
+			dfe.save();
+			
+			addLog ("Falha ao completar o registro do evento. " + event.getProcessMsg());
 		}
-		
-		return "@Success@";
-	}	//	doIt
+	}	//	manifest
 }	//	ManifestDFe
