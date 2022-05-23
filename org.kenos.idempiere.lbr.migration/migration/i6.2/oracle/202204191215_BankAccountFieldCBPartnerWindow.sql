f680c6e392a8e0d445ac81086c7e8e14563760e6
Davi Fochi <dfochi@kenos.com.br>
2022-05-23 18:28:30 -0300

    Fix Oracle Migration Scripts

--- org.kenos.idempiere.lbr.migration/migration/i6.2/oracle/202204191215_BankAccountFieldCBPartnerWindow.sql
+++ org.kenos.idempiere.lbr.migration/migration/i6.2/oracle/202204191215_BankAccountFieldCBPartnerWindow.sql
@@ -2,8 +2,7 @@ SET SQLBLANKLINES ON
 SET DEFINE OFF
 
 -- 19 de abr de 2022 12:10:20 BRT
-UPDATE AD_Field SET DisplayLogic='@lbr_PaymentRule@=''B'' | @lbr_PaymentRule@=''D'' | @lbr_PaymentRule@=15 | @lbr_PaymentRule@=16', AD_Reference_Value_ID=NULL, AD_Val_Rule_ID=NULL, IsToolbarButton=NULL,Updated>
+UPDATE AD_Field SET DisplayLogic='@lbr_PaymentRule@=''B'' | @lbr_PaymentRule@=''D'' | @lbr_PaymentRule@=15 | @lbr_PaymentRule@=16', AD_Reference_Value_ID=NULL, AD_Val_Rule_ID=NULL, IsToolbarButton=NULL,Updated=TO_DATE('2022-04-19 12:10:20','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=1000871
 ;
 SELECT Register_Migration_Script ('202204191215_BankAccountFieldCBPartnerWindow.sql') FROM DUAL
 ;
-
