6111a2f05550d5cbcfa8104e680e65bed7be6c59
Ricardo Santana <rsantana@kenos.com.br>
2023-04-27 16:36:20 -0300

    Add support for Tax Benefit Code     - Enable field to be shown without scale flag

---
+++ org.kenos.idempiere.lbr.migration/migration/i6.2/postgresql/202304271634_TaxBenefCode.sql
@@ -0,0 +1,7 @@
+-- 27 de abr de 2023 16:32:49 BRT
+UPDATE AD_Field SET DisplayLogic=NULL, AD_Reference_Value_ID=NULL, AD_Val_Rule_ID=NULL, IsToolbarButton=NULL,Updated=TO_TIMESTAMP('2023-04-27 16:32:48','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=1127365
+;
+
+SELECT Register_Migration_Script ('202304271634_TaxBenefCode.sql') FROM DUAL
+;
+
