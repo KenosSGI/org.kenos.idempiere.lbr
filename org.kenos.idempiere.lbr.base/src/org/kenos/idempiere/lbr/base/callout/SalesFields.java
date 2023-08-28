60c96d0202d9d0cbbd27d441bf973befc1993c7d
Ricardo Santana <rsantana@kenos.com.br>
2023-08-28 17:44:43 -0300

    fill nf description for invoices and purchase orders

--- org.kenos.idempiere.lbr.base/src/org/kenos/idempiere/lbr/base/callout/SalesFields.java
+++ org.kenos.idempiere.lbr.base/src/org/kenos/idempiere/lbr/base/callout/SalesFields.java
@@ -6,11 +6,9 @@ import org.adempiere.base.IColumnCallout;
 import org.adempiere.model.POWrapper;
 import org.adempierelbr.wrapper.I_W_C_BPartner;
 import org.adempierelbr.wrapper.I_W_C_Invoice;
-import org.adempierelbr.wrapper.I_W_C_Order;
 import org.compiere.model.GridField;
 import org.compiere.model.GridTab;
 import org.compiere.model.MBPartner;
-import org.compiere.model.MBankAccount;
 import org.compiere.model.MInvoice;
 import org.compiere.model.MOrder;
 
@@ -30,8 +28,7 @@ public class SalesFields implements IColumnCallout
 		Integer C_BPartner_ID = (Integer) mTab.getValue (MInvoice.COLUMNNAME_C_BPartner_ID);
 
 		if (C_BPartner_ID == null 
-				|| C_BPartner_ID.intValue() <= 0
-				|| !mTab.getValueAsBoolean(MInvoice.COLUMNNAME_IsSOTrx))
+				|| C_BPartner_ID.intValue() <= 0)
 			return "";
 
 		I_W_C_BPartner bpW = POWrapper.create(new MBPartner (ctx, C_BPartner_ID, null), I_W_C_BPartner.class);
@@ -40,15 +37,19 @@ public class SalesFields implements IColumnCallout
 		if (bpW.getlbr_PaymentRule() != null)
 			mTab.setValue(I_W_C_BPartner.COLUMNNAME_lbr_PaymentRule, bpW.getlbr_PaymentRule());
 		
-		//	Bank Account
-		int C_BankAccount_ID = bpW.getC_BankAccount_ID();
-		if (C_BankAccount_ID > 0)
+		//	Description
+		if (bpW.getlbr_NFDescription() != null)
 		{
-			Object org = mTab.getValue(I_W_C_Order.COLUMNNAME_AD_Org_ID);
-			//
-			MBankAccount ba = new MBankAccount (ctx, C_BankAccount_ID, null);
-			if (org != null && ((Integer)org).intValue() == ba.getAD_Org_ID())
-				mTab.setValue(I_W_C_BPartner.COLUMNNAME_C_BankAccount_ID, C_BankAccount_ID);
+			String nfDescription = (String) mTab.getValue(I_W_C_BPartner.COLUMNNAME_lbr_NFDescription);
+			if (nfDescription == null || nfDescription.isBlank())
+				nfDescription = "";
+			else
+				nfDescription += ". ";
+			
+			//	Do not duplicate
+			if (nfDescription.indexOf(bpW.getlbr_NFDescription()) == -1)
+				nfDescription += bpW.getlbr_NFDescription();
+			mTab.setValue(I_W_C_BPartner.COLUMNNAME_lbr_NFDescription, nfDescription);
 		}
 		
 		//	Orders only
@@ -57,10 +58,6 @@ public class SalesFields implements IColumnCallout
 			//	Shipper
 			if (bpW.getM_Shipper_ID() > 0)
 				mTab.setValue(I_W_C_BPartner.COLUMNNAME_M_Shipper_ID, bpW.getM_Shipper_ID());
-			
-			//	Description
-			if (bpW.getlbr_NFDescription() != null)
-				mTab.setValue(I_W_C_BPartner.COLUMNNAME_lbr_NFDescription, bpW.getlbr_NFDescription());
 			
 			//	Freight Cost Rule
 			if (bpW.getLBR_FreightCostRule() != null)
