69a82d192f6c3bf9c35399c759a7b01b19d66342
Ricardo Santana <rsantana@kenos.com.br>
2023-10-25 15:18:57 -0300

    include intercompany document type

---
+++ org.kenos.idempiere.lbr.migration/migration/i6.2/postgresql/202310251516_DocBaseLBR_Intercompany.sql
@@ -0,0 +1,12 @@
+-- 25 de out de 2023 15:09:12 BRT
+INSERT INTO AD_Ref_List (AD_Ref_List_ID,Name,AD_Reference_ID,Value,AD_Client_ID,AD_Org_ID,IsActive,Created,CreatedBy,Updated,UpdatedBy,EntityType,AD_Ref_List_UU) VALUES (1123015,'Intercompany Invoice AR',1000043,'FAEE-',0,0,'Y',TO_TIMESTAMP('2023-10-25 15:09:11','YYYY-MM-DD HH24:MI:SS'),100,TO_TIMESTAMP('2023-10-25 15:09:11','YYYY-MM-DD HH24:MI:SS'),100,'LBRA','d6131a1f-ec3b-40be-862c-f93f13c1c312')
+;
+
+-- 25 de out de 2023 15:09:21 BRT
+INSERT INTO AD_Ref_List (AD_Ref_List_ID,Name,AD_Reference_ID,Value,AD_Client_ID,AD_Org_ID,IsActive,Created,CreatedBy,Updated,UpdatedBy,EntityType,AD_Ref_List_UU) VALUES (1123016,'Intercompany Invoice AP',1000043,'FAEE+',0,0,'Y',TO_TIMESTAMP('2023-10-25 15:09:21','YYYY-MM-DD HH24:MI:SS'),100,TO_TIMESTAMP('2023-10-25 15:09:21','YYYY-MM-DD HH24:MI:SS'),100,'LBRA','e7fbd284-7bef-47dc-b798-3d885467c8b0')
+;
+
+-- 25 de out de 2023 15:09:21 BRT
+SELECT Register_Migration_Script ('202310251516_DocBaseLBR_Intercompany.sql') FROM DUAL
+;
+
